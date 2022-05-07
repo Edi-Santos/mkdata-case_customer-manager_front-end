bb988e899a0b5d8b6458196082d34eaee65a6a93
Ed Santos <edsantos2822@gmail.com>
2022-05-07 12:01:07 -0300

    build:(Form.jsx): implementando componente

---
+++ src/components/login/Form.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+import InputName from './InputName';
+import InputPassword from './InputPassword';
+
+function Form() {
+  const [loginDatas, setLoginDatas] = useState({
+    name: 'Admin',
+    password: '#admin2@#',
+  });
+
+  // Função para alterar o estado apenas para retirar mensagens de warning do console
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+
+    setLoginDatas((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const { name, password } = loginDatas;
+
+  return (
+    <form>
+      <InputName name={ name } onChange={ handleChange } />
+      <InputPassword password={ password } onChange={ handleChange } />
+    </form>
+  );
+}
+
+export default Form;
