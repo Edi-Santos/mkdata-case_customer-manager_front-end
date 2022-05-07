b3bc6c72e7ed79c9593f70ed07dd45ba95308e69
Ed Santos <edsantos2822@gmail.com>
2022-05-07 13:32:32 -0300

    build:(ButtonLogin.jsx): implementando componente

---
+++ src/components/login/ButtonLogin.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function ButtonLogin() {
+  return (
+    <Link to="/customers">
+      LOGIN
+    </Link>
+  );
+}
+
+export default ButtonLogin;
