53843d0c921ad95fa5635d63d6153d41b4d54ca5
Ed Santos <edsantos2822@gmail.com>
2022-05-07 11:58:21 -0300

    build:(Login.jsx): implementando página de login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Form from '../components/login/Form';
+
+function Login() {
+  return (
+    <main>
+      <Form />
+    </main>
+  );
+}
+
+export default Login;
