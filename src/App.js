d4b3d6fdc4de0863cdf2b6412c6acb603ff9f419
Ed Santos <edsantos2822@gmail.com>
2022-05-07 11:02:09 -0300

    feat:(App.js): adicionando rota para login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div>
-      Hello World!
-    </div>
+    <Routes>
+      <Route path="/login" element={ <Login /> } />
+    </Routes>
   );
 }
 
