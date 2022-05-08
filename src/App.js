bacf68bc565360c23ada704d5d14bcfca0a7aefe
Ed Santos <edsantos2822@gmail.com>
2022-05-07 21:25:53 -0300

    feat:(App.js): adicionando rota para customers

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 import Login from './pages/login/Login';
+import Customers from './pages/customers/Customers';
 
 import './App.css';
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <Routes>
       <Route path="/login" element={ <Login /> } />
+      <Route path="/customers" element={ <Customers /> } />
       <Route
         path="/"
         element={ <Navigate replace to="/login" /> }
