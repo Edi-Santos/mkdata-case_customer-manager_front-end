4ab2e78037930c8efa6d2ba4dc60ceba6fcbabc6
Ed Santos <edsantos2822@gmail.com>
2022-05-08 01:21:22 -0300

    build:(Customers.jsx): implementando página para customers

---
+++ src/pages/customers/Customers.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import Loading from '../loading/Loading';
+
+function Customers() {
+  const [isLoading, setIsloading] = useState(true);
+
+  const milliseconds = 2000;
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsloading(false);
+    }, milliseconds);
+  });
+
+  return (
+    <div>
+      {
+        isLoading ? <Loading />
+          : <h1>meu pastel é mais barato</h1>
+      }
+    </div>
+  );
+}
+
+export default Customers;
