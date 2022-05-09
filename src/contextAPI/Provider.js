7c89e6fd681618aaaf186483d962df1ad784e4ce
Ed Santos <edsantos2822@gmail.com>
2022-05-09 01:36:08 -0300

    feat:(dir:contextAPI): criando Context API para gerenciamento de estado global

---
+++ src/contextAPI/Provider.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+
+import request from '../services/fetchToAPI';
+
+import MyContext from './MyContext';
+
+function Provider({ children }) {
+  const [allCustomers, setAllCustomers] = useState([]);
+
+  useEffect(() => {
+    const getingCustomers = async () => {
+      const localUrl = 'http://localhost:3001/customer';
+      // const CloudUrl = 'https://mkdata-case-2822.herokuapp.com/customer'
+      const method = 'GET';
+      const headers = {
+        'Content-type': 'application.json; charset=UTF8',
+      };
+
+      const { customers } = await request(localUrl, method, headers);
+
+      setAllCustomers((prevState) => ({
+        ...prevState,
+        customers,
+      }));
+    };
+
+    getingCustomers();
+  }, []);
+
+  const contextValue = {
+    allCustomers,
+  };
+
+  return (
+    <MyContext.Provider value={ contextValue }>
+      { children }
+    </MyContext.Provider>
+  );
+}
+
+Provider.propTypes = {
+  children: PropTypes.shape({}).isRequired,
+};
+
+export default Provider;
