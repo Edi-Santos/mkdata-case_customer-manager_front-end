7591b291567b0d1eb014374ace611f815bd1c93e
Ed Santos <edsantos2822@gmail.com>
2022-05-09 16:36:42 -0300

    build:(Tbody.jsx): implementando componente

---
+++ src/components/customers/Tbody.jsx
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react';
+
+import MyContext from '../../contextAPI/MyContext';
+
+function Tbody() {
+  const { allCustomers: { customers } } = useContext(MyContext);
+
+  return customers.map((customer, index) => (
+    <tr key={ index }>
+      <td>{ customer.nome }</td>
+      <td>{ customer.grupo }</td>
+      <td>{ customer.ativo ? 'ativo' : 'inativo' }</td>
+      <td>X</td>
+    </tr>
+  ));
+}
+
+export default Tbody;
