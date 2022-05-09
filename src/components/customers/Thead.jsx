fb87075a3a7b6f2fda66987c5fad3e789075f18d
Ed Santos <edsantos2822@gmail.com>
2022-05-09 16:31:06 -0300

    build:(Thead.jsx): implementando componente

---
+++ src/components/customers/Thead.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Thead() {
+  return (
+    <tr>
+      <th>Nome</th>
+      <th>Grupo</th>
+      <th>Ativo</th>
+      <th>Deletar</th>
+    </tr>
+  );
+}
+
+export default Thead;
