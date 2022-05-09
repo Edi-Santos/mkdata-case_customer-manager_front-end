dea44df7493c0bdc06d82ce3c5807bb9906c01cd
Ed Santos <edsantos2822@gmail.com>
2022-05-09 04:24:57 -0300

    feat:(index.js): encapsulando aplicação no <Provider />

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Provider from './contextAPI/Provider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <Provider>
+        <App />
+      </Provider>
     </React.StrictMode>
   </BrowserRouter>
 );
