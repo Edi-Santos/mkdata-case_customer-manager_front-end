342ef1c431497bd1869a0360000b2afb9dafac85
Ed Santos <edsantos2822@gmail.com>
2022-05-10 13:31:04 -0300

    build:(Main.jsx): adicionando componente <UpdateCustomer />

--- src/components/customerDetails/Main.jsx
+++ src/components/customerDetails/Main.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 import Details from './Details';
 import BackToCustomers from './BackToCustomers';
+import UpdateCustomer from './UpdateCustomer';
 
 function Main() {
   return (
     <main>
       <Details />
+      <div>
+        <UpdateCustomer />
+      </div>
       <BackToCustomers />
     </main>
   );
