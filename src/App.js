e3bb159a41d2886e8004deca2e3a914ea6a59624
Ed Santos <edsantos2822@gmail.com>
2022-05-09 21:37:31 -0300

    feat:(App.js): aidiconando rota de detalhes do cliente

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 
 import Login from './pages/login/Login';
 import Customers from './pages/customers/Customers';
+import CustomerDetails from './pages/customerDetails/CustomerDetails';
 
 import './App.css';
 
@@ -11,6 +12,7 @@ function App() {
     <Routes>
       <Route path="/login" element={ <Login /> } />
       <Route path="/customers" element={ <Customers /> } />
+      <Route path="/customer/:id" element={ <CustomerDetails /> } />
       <Route
         path="/"
         element={ <Navigate replace to="/login" /> }
