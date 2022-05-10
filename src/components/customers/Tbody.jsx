f5e111613ff492bfb1be4bf1e93aea0ec88ac2ef
Ed Santos <edsantos2822@gmail.com>
2022-05-09 21:15:59 -0300

    feat/refactor:(Tbody.jsx): aidiconando evento de click e refatorando

--- src/components/customers/Tbody.jsx
+++ src/components/customers/Tbody.jsx
@@ -1,17 +1,24 @@
 import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import MyContext from '../../contextAPI/MyContext';
 
 function Tbody() {
   const { allCustomers: { customers } } = useContext(MyContext);
 
-  return customers.map((customer, index) => (
-    <tr key={ index }>
-      <td>{ customer.nome }</td>
-      <td>{ customer.grupo }</td>
-      <td>{ customer.ativo ? 'ativo' : 'inativo' }</td>
-    </tr>
-  ));
+  const navigate = useNavigate();
+
+  return customers.map((customer, index) => {
+    const { id, nome, grupo, ativo } = customer;
+
+    return (
+      <tr key={ index } onClick={ () => navigate(`/customer/${id}`) }>
+        <td>{ nome }</td>
+        <td>{ grupo }</td>
+        <td>{ ativo ? 'ativo' : 'inativo' }</td>
+      </tr>
+    );
+  });
 }
 
 export default Tbody;
