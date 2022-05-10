22a28d75989ab846eaa26d0525c4755c5db19faa
Ed Santos <edsantos2822@gmail.com>
2022-05-10 13:23:41 -0300

    build:(BackToCustomers.jsx): implementando componente

---
+++ src/components/customerDetails/BackToCustomers.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function BackToCustomers() {
+  return (
+    <Link to="/customers">
+      VOLTAR
+    </Link>
+  );
+}
+
+export default BackToCustomers;
