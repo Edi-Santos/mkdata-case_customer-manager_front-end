ca13b49bed869628a126aea8a149b18dcade55a2
Ed Santos <edsantos2822@gmail.com>
2022-05-11 16:03:53 -0300

    build:(Table.jsx): adicionando imagens

--- src/components/customers/Table.jsx
+++ src/components/customers/Table.jsx
@@ -3,18 +3,25 @@ import React from 'react';
 import Thead from './Thead';
 import Tbody from './Tbody';
 
+import injured from '../../images/undraw_injured_9757.svg';
+import medicine from '../../images/undraw_medicine_b-1-ol.svg';
+
 import './table.css';
 
 function Table() {
   return (
-    <table>
-      <thead>
-        <Thead />
-      </thead>
-      <tbody>
-        <Tbody />
-      </tbody>
-    </table>
+    <section>
+      <img src={ injured } alt="" />
+      <table>
+        <thead>
+          <Thead />
+        </thead>
+        <tbody>
+          <Tbody />
+        </tbody>
+      </table>
+      <img src={ medicine } alt="" />
+    </section>
   );
 }
 
